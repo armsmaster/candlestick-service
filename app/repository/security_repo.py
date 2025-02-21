648aeafe129b592efda305a9b84469a92d3f529c
Edward <edward.gordin@gmail.com>
2025-02-21 15:31:39 +0300

    reworked security repo: switched to sa core

--- app/repository/security_repo.py
+++ app/repository/security_repo.py
@@ -1,26 +1,29 @@
-from sqlalchemy import insert, select
+from uuid import uuid4
+from sqlalchemy import insert, select, delete, and_, or_
 
 from app.core.entities import Security
 from app.core.repository import ISecurityRepository
 
 from app.repository.base_repo import BaseRepository
-from app.repository.models import Security as SecurityModel
+from app.repository.metadata import security_table
 
 
 class SecurityRepository(BaseRepository, ISecurityRepository):
 
     def create(self, items: list[Security]):
-        for item in items:
-            existing = self.session.execute(
-                select(SecurityModel).filter_by(
-                    ticker=item.ticker,
-                    board=item.board,
-                )
-            ).scalar_one_or_none()
-            if existing is None:
-                security_model = SecurityModel(ticker=item.ticker, board=item.board)
-                self.session.add(security_model)
+        filter = self.__construct_filter(items)
+        existing_set = set(self.select_entities(filter=filter))
 
+        items_to_insert = [item for item in items if item not in existing_set]
+        if not items_to_insert:
+            return
+
+        insert_stmt = insert(security_table)
+        items_to_insert = [
+            {"id": uuid4(), "ticker": item.ticker, "board": item.board}
+            for item in items_to_insert
+        ]
+        self.connection.execute(insert_stmt, items_to_insert)
         return
 
     def update(self, items: list[Security]):
@@ -28,16 +31,28 @@ class SecurityRepository(BaseRepository, ISecurityRepository):
         raise NotImplementedError
 
     def delete(self, items: list[Security]):
-        for item in items:
-            security_model = self.session.execute(
-                select(SecurityModel).filter_by(
-                    ticker=item.ticker,
-                    board=item.board,
-                )
-            ).scalar_one_or_none()
-            if security_model is not None:
-                self.session.delete(security_model)
+        filter = self.__construct_filter(items)
+        delete_stmt = delete(security_table).where(filter)
+        self.connection.execute(delete_stmt)
 
     def all(self) -> list[Security]:
-        security_models = self.session.execute(select(SecurityModel)).scalars()
-        return [Security(ticker=sm.ticker, board=sm.board) for sm in security_models]
+        return self.select_entities()
+
+    def __construct_filter(self, items: list[Security]):
+        cols = security_table.c
+        ands = [
+            and_(
+                cols.ticker == item.ticker,
+                cols.board == item.board,
+            )
+            for item in items
+        ]
+        return or_(*ands)
+
+    def select_entities(self, filter=None) -> list[Security]:
+        where_clause = filter if filter is not None else 1 == 1
+        existing_stmt = select(security_table.c["ticker", "board"]).where(where_clause)
+        return {
+            Security(ticker=ticker, board=board)
+            for ticker, board in self.connection.execute(existing_stmt)
+        }
