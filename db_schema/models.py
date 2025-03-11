d70db5f051d32959f13a26ba14de82559408fb5f
Edward <edward.gordin@gmail.com>
2025-03-11 16:43:02 +0300

    updated schema: add candle_span model

--- db_schema/models.py
+++ db_schema/models.py
@@ -1,17 +1,21 @@
-from datetime import datetime
+from datetime import datetime, date
 import uuid
 
 from sqlalchemy import ForeignKey, Index
-from sqlalchemy import UUID
-from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy import UUID, BIGINT, TIMESTAMP
+
+# from sqlalchemy.ext.asyncio import AsyncAttrs
 
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column, relationship
 
 
-class Base(AsyncAttrs, DeclarativeBase):
-    pass
+class Base(DeclarativeBase):
+    type_annotation_map = {
+        int: BIGINT,
+        datetime: TIMESTAMP(timezone=True),
+    }
 
 
 class Security(Base):
@@ -33,6 +37,24 @@ class Candle(Base):
     high: Mapped[float]
     low: Mapped[float]
     close: Mapped[float]
+
+
+class CandleSpan(Base):
+    __tablename__ = "candle_span"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
+    security_id: Mapped[uuid.UUID] = mapped_column(UUID, ForeignKey("security.id"))
+    timeframe: Mapped[str]
+    date_from: Mapped[date]
+    date_till: Mapped[date]
+
+
+Index(
+    "idx_security_unique",
+    Security.ticker,
+    Security.board,
+    unique=True,
+)
 
 
 Index(
