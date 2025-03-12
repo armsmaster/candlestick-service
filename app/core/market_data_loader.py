79d3acf4344ee23f837f749e97ffc70746eea36a
Edward <edward.gordin@gmail.com>
2025-03-12 11:44:12 +0300

    update market data loader interface

--- app/core/market_data_loader.py
+++ app/core/market_data_loader.py
@@ -4,7 +4,8 @@ from dataclasses import dataclass
 from app.core.date_time import Timestamp
 from app.core.entities import Security, Timeframe
 from app.core.market_data_adapter import IMarketDataAdapter
-from app.core.repository import ICandleRepository
+from app.core.repository.candle_repository import ICandleRepository
+from app.core.repository.candle_span_repository import ICandleSpanRepository
 
 
 @dataclass
@@ -22,6 +23,7 @@ class IMarketDataLoader(ABC):
         self,
         market_data_adapter: IMarketDataAdapter,
         candle_repository: ICandleRepository,
+        candle_span_repository: ICandleSpanRepository,
     ):
         pass
 
