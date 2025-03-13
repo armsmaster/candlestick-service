e1ee6709de39fea535b69ce5b2dfc215528e0793
Edward <edward.gordin@gmail.com>
2025-03-13 12:08:38 +0300

    reorganized iterable interfaces in repositories

--- app/core/repository/candle_span_repository.py
+++ app/core/repository/candle_span_repository.py
@@ -1,13 +1,22 @@
 from abc import ABC, abstractmethod
+from typing import override
 
 from app.core.date_time import Timestamp
 from app.core.entities import Timeframe
-from app.core.entities import Security
+from app.core.entities import Security, CandleSpan
 
-from app.core.repository.base import IRepository
+from app.core.repository.base import IRepository, Record
 
 
-class ICandleSpanRepository(IRepository, ABC):
+class ICandleSpanRepository(IRepository):
+
+    @abstractmethod
+    def __aiter__(self) -> "ICandleSpanRepository":
+        raise NotImplementedError
+
+    @abstractmethod
+    async def __anext__(self) -> Record[CandleSpan]:
+        raise NotImplementedError
 
     @abstractmethod
     def filter_by_security(self, security: Security) -> "ICandleSpanRepository":
