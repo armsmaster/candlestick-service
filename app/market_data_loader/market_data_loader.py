7f109954f787e480bdfd1f4223c1bcaf26878867
Edward <edward.gordin@gmail.com>
2025-03-25 17:17:26 +0300

    fixed market data loader bug

--- app/market_data_loader/market_data_loader.py
+++ app/market_data_loader/market_data_loader.py
@@ -1,19 +1,19 @@
+"""Market Data Loader implementation."""
+
 import asyncio
 
-from app.core.market_data_loader import IMarketDataLoader, MarketDataLoaderRequest
+from app.core.entities import CandleSpan, Security, Timeframe
 from app.core.market_data_adapter import IMarketDataAdapter, MarketDataRequest
-
-from app.core.date_time import Timestamp
-from app.core.entities import Security, Candle, CandleSpan, Timeframe
-from app.core.unit_of_work import IUnitOfWork
-from app.core.repository.base import Record
-from app.core.repository.security_repository import ISecurityRepository
+from app.core.market_data_loader import IMarketDataLoader, MarketDataLoaderRequest
 from app.core.repository.candle_repository import ICandleRepository
 from app.core.repository.candle_span_repository import ICandleSpanRepository
+from app.core.repository.security_repository import ISecurityRepository
+from app.core.unit_of_work import IUnitOfWork
 from app.market_data_loader.range_operations import Range, rangediff, rangemerge
 
 
 class MarketDataLoader(IMarketDataLoader):
+    """Market Data Loader."""
 
     def __init__(
         self,
@@ -23,6 +23,7 @@ class MarketDataLoader(IMarketDataLoader):
         candle_span_repository: ICandleSpanRepository,
         unit_of_work: IUnitOfWork,
     ):
+        """Initialize."""
         self.market_data_adapter = market_data_adapter
         self.security_repository = security_repository
         self.candle_repository = candle_repository
@@ -30,7 +31,10 @@ class MarketDataLoader(IMarketDataLoader):
         self.unit_of_work = unit_of_work
 
     async def load_candles(self, request: MarketDataLoaderRequest) -> None:
+        """Load candles."""
         request_batches = await self._construct_batches(request)
+        if not request_batches:
+            return
         await asyncio.gather(*[self._load_batch(rb) for rb in request_batches])
         candles = self.market_data_adapter.candles
         async with self.unit_of_work:
@@ -45,6 +49,7 @@ class MarketDataLoader(IMarketDataLoader):
         self,
         request: MarketDataLoaderRequest,
     ) -> list[MarketDataLoaderRequest]:
+        """Split request into batches to exclude loading data already present in DB."""
         repo = self.candle_span_repository.filter_by_security(
             request.security
         ).filter_by_timeframe(request.timeframe)
@@ -72,6 +77,11 @@ class MarketDataLoader(IMarketDataLoader):
         timeframe: Timeframe,
         batches: list[MarketDataLoaderRequest],
     ):
+        """
+        Update stored time spans.
+
+        Time spans represent periods, for which candles are stored in DB.
+        """
         repo = self.candle_span_repository
         repo = repo.filter_by_security(security).filter_by_timeframe(timeframe)
         span_records = [span_record async for span_record in repo]
@@ -94,6 +104,7 @@ class MarketDataLoader(IMarketDataLoader):
         await self.candle_span_repository.add(updated_spans)
 
     async def _load_batch(self, request: MarketDataLoaderRequest) -> None:
+        """Load market data."""
         md_request = MarketDataRequest(
             board=request.security.board,
             ticker=request.security.ticker,
