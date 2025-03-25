3a189e7681611a2444d4eacbe61d1d2ec890716c
Edward <edward.gordin@gmail.com>
2025-03-25 11:36:49 +0300

    created base classes for use case package

---
+++ app/use_cases/base.py
@@ -0,0 +1,40 @@
+"""Base classes for Use Case component."""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class UseCaseEvent:
+    """Base UseCase Event."""
+
+    pass
+
+
+@dataclass
+class UseCaseRequest:
+    """Base UseCase Request."""
+
+    pass
+
+
+@dataclass
+class UseCaseResponse:
+    """Base UseCase Response."""
+
+    result: Any | None = None
+    errors: list[str] = list()
+
+
+class BaseUseCase(ABC):
+    """Base class for use cases."""
+
+    @abstractmethod
+    async def execute(self, request: UseCaseRequest) -> UseCaseResponse:
+        """Execute use case."""
+        raise NotImplementedError
+
+    async def log_event(self, event: UseCaseEvent) -> None:
+        """Log use case event."""
+        pass
