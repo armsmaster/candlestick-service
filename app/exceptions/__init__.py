5f2a82ee8f2dbcf5b77d5dcb9122169992efb0a8
Edward <edward.gordin@gmail.com>
2025-04-02 12:10:24 +0300

    refactor exception handling

---
+++ app/exceptions/__init__.py
@@ -0,0 +1,7 @@
+__all__ = [
+    "DatabaseException",
+    "MarketDataSourceException",
+]
+
+from app.exceptions.database_exception import DatabaseException
+from app.exceptions.market_data_source_exception import MarketDataSourceException
---
+++ app/exceptions/market_data_source_exception.py
@@ -0,0 +1,2 @@
+class MarketDataSourceException(Exception):
+    pass
