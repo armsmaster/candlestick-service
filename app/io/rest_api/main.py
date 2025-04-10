851db035384252450c43d21a3cd1237c8a5272e5
Edward <edward.gordin@gmail.com>
2025-04-10 10:39:25 +0300

    add project info to swagger ui

--- app/io/rest_api/main.py
+++ app/io/rest_api/main.py
@@ -2,5 +2,12 @@ from fastapi import FastAPI
 
 from app.io.rest_api.api import router as router_api
 
-app = FastAPI()
+app = FastAPI(
+    title="Candlestick Service",
+    contact={
+        "name": "Edward Gordin",
+        "url": "https://github.com/armsmaster/candlestick-service",
+        "email": "edward.gordin@gmail.com",
+    },
+)
 app.include_router(router_api)
